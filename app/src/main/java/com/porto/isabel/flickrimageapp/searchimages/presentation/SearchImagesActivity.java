c24c578dc15063206b48d968a460bf7f19cd054e
Isabel Porto <belporto@gmail.com>
2017-07-08 16:20:20 +0100

    Show search view

--- app/src/main/java/com/porto/isabel/flickrimageapp/searchimages/presentation/SearchImagesActivity.java
+++ app/src/main/java/com/porto/isabel/flickrimageapp/searchimages/presentation/SearchImagesActivity.java
@@ -1,10 +1,17 @@
 package com.porto.isabel.flickrimageapp.searchimages.presentation;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.porto.isabel.flickrimageapp.AppApplication;
 import com.porto.isabel.flickrimageapp.R;
@@ -39,6 +46,10 @@ public class SearchImagesActivity extends AppCompatActivity implements SearchIma
         setContentView(R.layout.activity_main);
         mPresenter.onCreate();
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.photos_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(null);
+
         mRecyclerView = (RecyclerView) findViewById(R.id.photos_recycler_view);
 
         RecyclerView.LayoutManager layoutManager
@@ -48,6 +59,45 @@ public class SearchImagesActivity extends AppCompatActivity implements SearchIma
         mRecyclerView.setHasFixedSize(true);
         mSearchImagesAdapter = new SearchImagesAdapter();
         mRecyclerView.setAdapter(mSearchImagesAdapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.image_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        SearchView searchView = null;
+        if (searchItem != null) {
+            searchView = (SearchView) searchItem.getActionView();
+        }
+        if (searchView != null) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+            //searchView.setIconifiedByDefault(false);
+
+            SearchView.OnQueryTextListener queryTextListener = new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    Log.i("onQueryTextChange", newText);
+
+                    return true;
+                }
+
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    Log.i("onQueryTextSubmit", query);
+
+                    return true;
+                }
+            };
+            searchView.setOnQueryTextListener(queryTextListener);
+        }
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
